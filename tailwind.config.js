7d2fac0c5b8ae113dcb11d83e8a86757e223af0b
Matthew Selby <56660100+mattselby922@users.noreply.github.com>
2021-06-27 13:45:43 -0400

    building out the landing page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// Custom tailwind config 
+module.exports = {
+    theme: {
+        extend: {
+            backgroundImage: theme => ({
+                'tree-background': "url('/src/Group.png')",
+                'blue-background': "url('/src/Bitmap.png')",
+            })
+        }
+    }
+}
